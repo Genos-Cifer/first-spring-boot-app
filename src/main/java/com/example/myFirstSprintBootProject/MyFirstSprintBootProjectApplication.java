c74d1cfb9310416a10ef7fdf57e144127391af23
Genos-Cifer <utkarshmkhare@gmail.com>
2023-03-07 01:41:35 +0530

    initial commit

---
+++ src/main/java/com/example/myFirstSprintBootProject/MyFirstSprintBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myFirstSprintBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSprintBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSprintBootProjectApplication.class, args);
+	}
+
+}
